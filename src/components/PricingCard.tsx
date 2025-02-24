73bb43d0e30766e6a2fb0cd3db32d54c6efbbf1b
Jenny Pieloor <devjenny.official@gmail.com>
2025-02-24 23:25:33 +0800

    Remove animation.

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -4,11 +4,9 @@ import { FaCheck } from "react-icons/fa";
 import { LuChevronDown } from "react-icons/lu";
 import { pricingList } from "../constants/PricingData";
 import SharedButton from "./SharedButton";
-import useDeviceType from "@/hooks/useDeviceType";
 
 const PricingCard = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const { isMobile, isTablet } = useDeviceType();
 
   return (
     <div className="flex xl:flex-row flex-col xl:gap-[3px] md:gap-[1px] gap-7">
@@ -19,18 +17,6 @@ const PricingCard = () => {
             className={`z-${
               hoveredIndex === index ? "20" : "0"
             } sm:border-none border border-white focus:border-brand active:border-brand transition-default relative w-full xl:max-w-[310px] flex xl:flex-col md:flex-row flex-col xl:justify-start md:justify-between justify-start md:items-center xl:gap-6 md:gap-0 gap-6 md:shadow-xl shadow-sm xl:p-7 md:p-0 p-5 bg-white cursor-pointer`}
-            initial={isTablet ? { scale: 0.8 } : { opacity: 0, y: 100 }}
-            animate={
-              isTablet ? { scale: 1 } : isMobile ? {} : { y: 0, opacity: 1 }
-            }
-            whileInView={isMobile ? { opacity: 1, y: 0 } : {}}
-            transition={{
-              type: "spring",
-              bounce: 0.4,
-              duration: 0.9,
-              delay: isTablet ? index * 0.5 : index * 0.1,
-            }}
-            viewport={{ once: true, amount: 0.2 }}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 1.05 }}
             onMouseEnter={() => setHoveredIndex(index)}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,5 @@
 import { Switch } from "@/components/ui/switch";
 import PricingCard from "../components/PricingCard";
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
@@ -18,16 +17,7 @@ const Hero = () => {
   return (
     <div className="w-full flex justify-center items-center bg-gray/10 sm:!mt-16 !mt-7">
       <div className="w-full max-w-[1440px] mx-auto pt-23 pb-13 flex flex-col justify-center items-center gap-5 px-5">
-        <motion.div
-          initial={{ y: 60, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8,
-            delay: 0.1,
-          }}
-        >
+        <div>
           <p className="hidden xl:block max-w-[1107px] 2xl:text-5xl text-5xl font-bold tracking-tight leading-19">
             Convert More Visitors With The Popup Builder 500,000+ Websites Use
           </p>
@@ -46,24 +36,13 @@ const Hero = () => {
             <Switch defaultChecked />
             <p>Annually</p>
           </div>
-        </motion.div>
+        </div>
 
         <PricingCard />
 
-        <motion.div
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8,
-            delay: 0.5,
-          }}
-        >
-          <p className="text-lightGray font-medium !mt-8">
-            *All plans are billed annually
-          </p>
-        </motion.div>
+        <p className="text-lightGray font-medium !mt-8">
+          *All plans are billed annually
+        </p>
       </div>
     </div>
   );
